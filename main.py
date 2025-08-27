3cf7156cc39a0805d8668052e84ac3fb677575a3
mgkim709 <mgkim321@ajou.ac.kr>
2025-08-27 17:11:41 +0900

    check

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     print("Hello from mp!")
     print(f"Torch version: {torch.__version__}")
     print(torch.cuda.is_available())
+    print(torch.cuda.current_device())
     
 if __name__ == "__main__":
     main()
