a69098a01e44ca91739b46b582a4ae93f77b577f
mgkim709 <mgkim321@ajou.ac.kr>
2025-08-27 16:37:27 +0900

    Initial commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 def main():
     print("Hello from mp!")
     print(f"Torch version: {torch.__version__}")
-
+    print(torch.cuda.is_available())
+    
 if __name__ == "__main__":
     main()
